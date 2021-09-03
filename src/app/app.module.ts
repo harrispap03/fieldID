fee62c0ce1bb9922d07a4b0f6d60b95d4d2d0ab0
harry <harrispap03@gmail.com>
2021-09-03 12:36:22 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddNewUserComponent } from './add-new-user/add-new-user.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
+import { RecordComponent } from './record/record.component';
+import { AngularFireModule } from '@angular/fire';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddNewUserComponent,
+    NavigationBarComponent,
+    QrScannerComponent,
+    RecordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
