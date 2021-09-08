95283479a7f297deb139d6e5a620829f157750f6
harry <harrispap03@gmail.com>
2021-09-08 15:48:41 +0300

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { AddNewUserComponent } from './add-new-user/add-new-user.component';
 import { RecordComponent } from './record/record.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'qrScanner', component: QrScannerComponent },
-  { path: 'newUser', component: AddNewUserComponent },
+  { path: 'addNewUser', component: AddNewUserComponent },
   { path: 'record', component: RecordComponent },
+  { path: '**', component: HomeComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
