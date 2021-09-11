242db0392d42612a6a03d21c234474cf51eb2620
harry <harrispap03@gmail.com>
2021-09-11 14:17:37 +0300

    Added add-new-user

--- src/app/add-new-user/add-new-user.component.ts
+++ src/app/add-new-user/add-new-user.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-add-new-user',
   templateUrl: './add-new-user.component.html',
-  styleUrls: ['./add-new-user.component.scss']
+  styleUrls: ['./add-new-user.component.scss'],
 })
 export class AddNewUserComponent implements OnInit {
+  newUserForm!: FormGroup;
+  userId!: string;
+  qrCodeCreated = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private afs: AngularFirestore) {}
 
   ngOnInit(): void {
+    this.newUserForm = this.fb.group({
+      name: [null, [Validators.required]],
+      surname: [null, [Validators.required]],
+      idNum: [null, [Validators.required]],
+    });
   }
 
+  async onSubmit() {
+    const formValue = this.newUserForm.value;
+
+    try {
+      await this.afs
+        .collection('users')
+        .add(formValue)
+        .then((user) => (this.userId = user.id));
+    } catch (err) {
+      console.error(err);
+    }
+    this.qrCodeCreated = true;
+  }
 }
