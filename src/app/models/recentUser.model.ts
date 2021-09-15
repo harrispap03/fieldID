12374c1f195ae6bc79cbffc62af62682cc378cc9
harry <harrispap03@gmail.com>
2021-09-15 10:47:56 +0300

    adding QR scanner

--- src/app/models/recentUser.model.ts
+++ src/app/models/recentUser.model.ts
@@ -1,5 +1,5 @@
 import { User } from "./user.model";
 
 export interface RecentUser extends User{
-	checkInTime: string;
+	checkInTime?: Date
 }
--- src/app/qr-scanner/qr-scanner.component.ts
+++ src/app/qr-scanner/qr-scanner.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable, Subject, Subscriber, Subscription } from 'rxjs';
+import { distinctUntilChanged, filter, mergeMap } from 'rxjs/operators';
+import { RecentUser } from '../models/recentUser.model';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-qr-scanner',
   templateUrl: './qr-scanner.component.html',
-  styleUrls: ['./qr-scanner.component.scss']
+  styleUrls: ['./qr-scanner.component.scss'],
 })
-export class QrScannerComponent implements OnInit {
+export class QrScannerComponent {
+  private qrResult$ = new Subject<string>();
+  addUserToAFS!: Subscription;
+  userData!: User;
+  private recentUserData!: RecentUser;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private afs: AngularFirestore) {
+    this.addUserToAFS = this.qrResult$
+      .pipe(
+        distinctUntilChanged(),
+        mergeMap((qrResult) =>
+          this.afs.collection<User>('users').doc(qrResult).valueChanges()
+        )
+      )
+      .subscribe((userData: any) => {
+        this.userData = userData;
+        this.recentUserData = this.userData;
+        this.recentUserData.checkInTime = new Date();
+        this.afs.collection('recentUsers').add(this.recentUserData);
+      });
   }
 
+  onCamerasNotFound(error: Error) {
+    console.log(error); // Replace this with some HTML
+  }
+
+  onScanSuccess(qrCode: string) {
+    this.qrResult$.next(qrCode);
+  }
+
+  // this.sub = this.qrResult$.pipe(
+  //   filter(id => !!id),
+  //   distinctUntilChanged(),
+  //   mergeMap((IncomingQRResult ) => this.afs.collection('users').doc(IncomingQRResult).valueChanges())
+  // ).subscribe( data => {
+  //   this.doc = data;
+  //   this.doc.checkInTime = new Date();
+  //   this.afs.collection('recentUsers').add(this.doc);
+  // });
 }
