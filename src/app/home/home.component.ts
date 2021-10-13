5508ee25b2e148b48068865a8a272f670fb2c9da
harry <harrispap03@gmail.com>
2021-10-13 11:41:30 +0300

    Handset home component now fully responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,17 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor() {}
+  isHandset$ = this.breakpointObserver.observe([Breakpoints.Handset]).pipe(
+    map((result) => result.matches),
+    shareReplay()
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
