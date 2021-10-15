48f1ddd90ce6e1988dec5a0f6f7eb437f2ca070c
harry <harrispap03@gmail.com>
2021-10-15 08:54:48 +0300

    added first animation, still needs better config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,37 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
-import { AddNewUserComponent } from './add-new-user/add-new-user.component';
 import { RecordComponent } from './record/record.component';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'qrScanner', component: QrScannerComponent },
-  { path: 'manageUsers', component: ManageUsersComponent },
-  { path: 'record', component: RecordComponent },
-  { path: '**', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { animation: 'HomePage' },
+  },
+  {
+    path: 'qrScanner',
+    component: QrScannerComponent,
+    data: { animation: 'ScannerPage' },
+  },
+  {
+    path: 'manageUsers',
+    component: ManageUsersComponent,
+    data: { animation: 'ManageUsersPage' },
+  },
+  {
+    path: 'record',
+    component: RecordComponent,
+    data: { animation: 'RecordPage' },
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
